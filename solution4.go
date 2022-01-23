3bc67bbf98b2e362328a7f1114eae8f18ea53c69
sonadztux <dev.sonadztux@gmail.com>
2022-01-24 00:35:37 +0700

    add solution for number 4

---
+++ solution4.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+	wordList := [...]string{"kita", "atik", "tika", "aku", "kia", "makan", "kua"}
+	fmt.Println(findAnagramWordInList(wordList[:]))
+}
+
+func findAnagramWordInList(wordList []string) [][]string {
+	anagramWords := make(map[string][]string)
+	var anagramWordsInSlice [][]string
+
+	for _, word := range wordList {
+		key := ""
+
+		wordSplitted := strings.Split(word, "")
+		sort.Strings(wordSplitted)
+		key += strings.Join(wordSplitted, "")
+		anagramWords[key] = append(anagramWords[key], word)
+	}
+
+	// fmt.Println(anagramWords)
+	for _, anagramWord := range anagramWords {
+		anagramWordsInSlice = append(anagramWordsInSlice, anagramWord)
+	}
+
+	return anagramWordsInSlice
+}
